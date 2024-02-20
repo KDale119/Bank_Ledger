02feb321f2c4ba000606fc7180b65757a01e55c1
Kayla Dale <kmeyers119@gmail.com>
2024-02-20 15:37:14 -0600

    sorrected statement generating fix format still

--- src/main/java/Reports.java
+++ src/main/java/Reports.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Reports {
@@ -14,6 +15,7 @@ public class Reports {
 
         System.out.print("\nPlease Enter Customer Name: ");
         String custName = scanner.nextLine();
+        String custNameTrim = custName.replaceAll("\\s", "");
 
         String currDate = LocalDateTime.now().toString();
 
@@ -25,45 +27,71 @@ public class Reports {
             connect = DriverManager.getConnection(url);
 
             // GETTING CustomerID
-            PreparedStatement statement = connect.prepareStatement("select ID from Customer where Name = (?)");
-            statement.setString(1, custName);
-            ResultSet rs = statement.executeQuery();
+            PreparedStatement custStatement = connect.prepareStatement("select ID from Customer where Name = (?)");
+            custStatement.setString(1, custName);
+            ResultSet rs = custStatement.executeQuery();
             String custID = rs.getString("ID");
 
 
             // GETTING ACCOUNTID FROM CustomerID
-            statement = connect.prepareStatement("select ID, Name, Balance from Account where CustomerID = (?)");
-            statement.setString(1, custID);
-            ResultSet custrs = statement.executeQuery();
-            String accID = custrs.getString("ID");
-            String accName = custrs.getString("Name");
-//            String accBalance = custrs.getString("Balance");
+            PreparedStatement accountID = connect.prepareStatement("select ID, Name, Balance from Account where CustomerID = (?)");
+            accountID.setString(1, custID);
+            ResultSet accRS = accountID.executeQuery();
+            String accID = accRS.getString("ID");
+            String accName = accRS.getString("Name");
+            String accBalance = accRS.getString("Balance");
 
             // GETTING TRANSACTIONS FROM AccountID
-            statement = connect.prepareStatement("select * from Trans where AccountID = (?)");
-            statement.setString(1, accID);
-            ResultSet transrs = statement.executeQuery();
+            PreparedStatement transStatement = connect.prepareStatement("select * from Trans where AccountID = (?)");
+            transStatement.setString(1, accID);
+            ResultSet transrs = transStatement.executeQuery();
 //            String transFullBalance = transrs.getString("Balance"); // make it to add total of accs
 
 
             // JAVA FILE
-            File bankStatement = new File(filePath + custName + ".txt");
+            File bankStatement = new File(filePath + "\\" + custNameTrim + ".txt");
             writer = new BufferedWriter(new FileWriter(bankStatement));
             writer.write("MCC Code School Bank Statement\n");
-            writer.write("\nStatement for " + custName);
+
+            writer.write("\nStatement for " + custName + "\n");
             writer.write("Statement Date: " + currDate + "\n");
-            writer.write("\n" + accName + "\n");
+
+            //Current total balance here
+
+            writer.write("\n" + accName + " - $" + accBalance + " Balance\n"); // needs to be from ACCOUNT
             writer.write("Transactions:\n");
-            writer.write(custrs.getString("Balance")); // needs to be from ACCOUNT
+
             while (transrs.next()) {
-                writer.write(transrs.getString("TransTime"));
-                writer.write(transrs.getString("Type"));
-                writer.write(transrs.getString("Amount"));
-                writer.write(transrs.getString("MerchantName"));
+                String time = transrs.getString("TransTime");
+                String type = transrs.getString("Type");
+                String amount = transrs.getString("Amount");
+                String merchantName = transrs.getString("MerchantName");
+                if (transrs.getString("MerchantName") != null) {
+                    writer.write(time + " ");
+                    writer.write(merchantName + " ");
+                    if (type.equalsIgnoreCase("debit")) {
+                        writer.write("-$");
+                    } else {
+                        writer.write("+$");
+                    }
+                    writer.write(amount + "\n");
+                } else {
+                    if (type.equals("C")) {
+                        writer.write(time + " ");
+                        writer.write("Deposit ");
+                        writer.write("+$" + amount + "\n");
+                    } else {
+                        writer.write(time + " ");
+                        writer.write("Withdrawl ");
+                        writer.write("-$" + amount + "\n");
+                    }
+                }
+
             }
 
         } catch (IOException e) {
             System.out.println("error 1");
+            e.printStackTrace();
         } catch (SQLException e) {
             System.out.println("error 2");
             e.printStackTrace();
